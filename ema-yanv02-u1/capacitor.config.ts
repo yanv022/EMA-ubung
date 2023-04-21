205176af34866b229f7edf490fabfcba2e1aebeb
yann ndemmeze <yann.abel.ndemmeze.vougue@mni.thm.de>
2023-04-21 17:44:20 +0200

    ubung1

---
+++ ema-yanv02-u1/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ema-yanv02-u1',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
