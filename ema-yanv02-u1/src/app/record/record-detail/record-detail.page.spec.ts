ff4a268c6106ea59617f6d56cd4246555fe46ed6
yann ndemmeze <yann.abel.ndemmeze.vougue@mni.thm.de>
2023-05-03 12:12:40 +0200

    update

--- ema-yanv02-u1/src/app/record/record-detail/record-detail.page.spec.ts
+++ ema-yanv02-u1/src/app/record/record-detail/record-detail.page.spec.ts
@@ -1,11 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import { RecordDetailPage } from './record-detail.page';
 
 describe('RecordDetailPage', () => {
   let component: RecordDetailPage;
   let fixture: ComponentFixture<RecordDetailPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(RecordDetailPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- ema-yanv02-u1/src/app/record/record-list/record-list.page.ts
+++ ema-yanv02-u1/src/app/record/record-list/record-list.page.ts
@@ -8,17 +8,12 @@ import {Statistic} from '../statistic.model';
   styleUrls: ['./record-list.page.scss'],
 })
 export class RecordListPage implements OnInit {
-  records: Record[] = [];
-  record0: Record[] = [];
-  record1: Record[] = [
+  records: Record[] = [
     new Record(1, 'CS1013', 'Objektorientierte Programmierung', 6, 73, true, true, 2016),
     new Record(2, 'MN1007', 'Diskrete Mathematik', 6, 81, true, false, 2016),
+    new Record(3, 'CS1019', 'Compilerbau', 6, 81, false, false, 2017),
+    new Record(4, 'CS1020', 'Datenbanksysteme', 6, 92, false, false, 2017)
   ];
-  record2: Record[] = [
-    new Record(1, 'CS1013', 'Objektorientierte Programmierung', 6, 73, true, true, 2016),
-    new Record(2, 'MN1007', 'Diskrete Mathematik', 6, 81, true, false, 2016),
-  ];
-
 
   constructor() {
 
@@ -27,12 +22,13 @@ export class RecordListPage implements OnInit {
   }
 
   showStats() {
+    this.records = [];
     console.log("showStats");
     console.log("statsNoRecords");
     console.log(new Statistic(this.records));
     this.records.push(
-      new Record(3, 'CS1019', 'Compilerbau', 6, 81, false, false, 2017),
-      new Record(4, 'CS1020', 'Datenbanksysteme', 6, 92, false, false, 2017)
+      new Record(1, 'CS1013', 'Objektorientierte Programmierung', 6, 73, true, true, 2016),
+      new Record(2, 'MN1007', 'Diskrete Mathematik', 6, 81, true, false, 2016),
     );
     console.log("stats2Records");
     console.log(new Statistic(this.records));
